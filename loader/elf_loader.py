e27aced5303d92db7c86efc8bd6456e164b08e6a
R4mnesia <post.xes42@passmail.net>
2026-02-13 11:15:25 +0100

    ELF64: Clean code and create new file read_section.py

--- loader/elf_loader.py
+++ loader/elf_loader.py
@@ -1,46 +1,12 @@
-from elftools.elf.elffile import ELFFile
 from capstone import Cs, CS_ARCH_X86, CS_MODE_64
 from capstone.x86 import *
 import elftools.common.utils as ecu
+from .read_section import *
 
 def elf_loader32(file):
     print("ELF 32 bits loader")
 
 # CS_OP_IMM = immediate operands
-
-def check_stripped(file):
-
-    try: 
-        with open(file, 'rb') as f:
-            elf = ELFFile(f)
-            symtab = elf.get_section_by_name('.symtab')
-            code = symtab.data()
-            addr = symtab['sh_addr']
-
-            for sym in symtab.iter_symbols():
-                if sym.name == "main":
-                    return sym.entry['st_value']
-    except:
-        return True
-
-
-def read_rodata(file):
-    with open(file, 'rb') as f:
-        elf = ELFFile(f)
-        rodata = elf.get_section_by_name('.rodata')
-        code = rodata.data()
-        base_addr = rodata['sh_addr']
-
-    print(f"\n{hex(base_addr)} @.rodata: \n{code}")
-    return base_addr, code
-
-def dump_hex(base_addr, data):
-    
-    for i in range(0, len(data), 16): # increment 16
-        chunk = data[i:i + 16]
-        hex_bytes = " ".join(f"{b:02x}" for b in chunk)
-        print(f"  {base_addr + i:08x}  {hex_bytes}")
-    print("")
 
 def elf_loader64(file):
     print("ELF 64 bits loader")
